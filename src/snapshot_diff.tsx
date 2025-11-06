13e2b24a7f11363358e6c34253569d4b496a18f4
Alice Brooks <alice.brooks@suse.com>
2025-11-06 13:48:56 +0000

    Rewrite snapshot_diff layout

--- src/snapshot_diff.tsx
+++ src/snapshot_diff.tsx
@@ -2,7 +2,7 @@ import cockpit from "cockpit";
 import React, { useState, useEffect } from "react";
 import { EmptyStatePanel } from 'cockpit-components-empty-state';
 import { useDialogs } from 'dialogs.jsx';
-import { Breadcrumb, BreadcrumbItem, Button, Card, CardBody, CardHeader, CardTitle, Modal, ModalBody, ModalHeader, PageBreadcrumb, PageSection } from "@patternfly/react-core";
+import { Accordion, AccordionContent, AccordionItem, AccordionToggle, Breadcrumb, BreadcrumbItem, Button, Card, CardBody, CardHeader, CardTitle, Modal, ModalBody, ModalHeader, PageBreadcrumb, PageSection } from "@patternfly/react-core";
 import { Snapshot, SndiffDiff, SndiffModifiedFiles, SnDiffModifiedPackages } from "./types";
 import { CompareDialog } from "./compare_dialog";
 
@@ -29,6 +29,7 @@ const SnapshotDiff = ({ pre_snapshot, post_snapshot }: { pre_snapshot: number, p
     const Dialogs = useDialogs();
     const [modifiedPackages, setModifiedPackages] = useState<SnDiffModifiedPackages | null>(null);
     const [modifiedFiles, setModifiedFiles] = useState<SndiffModifiedFiles | null>(null);
+    const [openAccordion, setOpenAccordion] = useState<string[]>([]);
 
     useEffect(() => {
         setModifiedPackages(null);
@@ -45,35 +46,91 @@ const SnapshotDiff = ({ pre_snapshot, post_snapshot }: { pre_snapshot: number, p
                         });
     }, [post_snapshot, pre_snapshot]);
 
+    const onAccordionToggle = (id: string) => {
+        if (openAccordion.includes(id)) {
+            setOpenAccordion(openAccordion.filter((item) => item !== id));
+        } else {
+            setOpenAccordion([...openAccordion, id]);
+        }
+    };
+
+    const accordionItem = (key: string, title: string, content: React.ReactNode) => {
+        return (
+            <AccordionItem isExpanded={openAccordion.includes(key)}>
+                <AccordionToggle
+                    onClick={() => {
+                        onAccordionToggle(key);
+                    }}
+                    id={key}
+                >
+                    {title}
+                </AccordionToggle>
+                <AccordionContent id={key}>
+                    {content}
+                </AccordionContent>
+            </AccordionItem>
+        );
+    };
+
     if (modifiedPackages === null || modifiedFiles === null)
         return <EmptyStatePanel loading />;
 
     return (
-        <>
-            <h3> {_("Packages")}</h3>
-            <p>{_("Updated")}: {modifiedPackages.updated.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}</p>
-            <p>{_("Downgraded")}: {modifiedPackages.downgraded.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}</p>
-            <p>{_("Added")}: {modifiedPackages.added.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}</p>
-            <p>{_("Removed")}: {modifiedPackages.removed.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}</p>
+        <Accordion asDefinitionList>
+            {modifiedPackages.updated.length > 0 &&
+                accordionItem("def-updated-packages", _("Updated Packages"), (
+                    <p>
+                        {modifiedPackages.updated.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}
+                    </p>
+                ))}
+            {modifiedPackages.downgraded.length > 0 &&
+                accordionItem("def-downgraded-packages", _("Downgraded Packages"), (
+                    <p>
+                        {modifiedPackages.downgraded.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}
+                    </p>
+                ))}
+            {modifiedPackages.added.length > 0 &&
+                accordionItem("def-added-packages", _("Added Packages"), (
+                    <p>
+                        {modifiedPackages.added.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}
+                    </p>
+                ))}
+            {modifiedPackages.removed.length > 0 &&
+                accordionItem("def-removed-packages", _("Removed Packages"), (
+                    <p>
+                        {modifiedPackages.removed.reduce((combined, item) => { combined += item.name + " "; return combined }, "")}
+                    </p>
+                ))}
 
-            <h3>{_("Files")}</h3>
-            <p>{_("Modified")}: {modifiedFiles.modified.map(item => {
-                if (!item.file_diff) {
-                    return item.path + " ";
-                }
-                return <span key={item.path}><a onClick={() => Dialogs.show(<DiffDialog file={item.path} diff={item.file_diff} />)}>{item.path}</a> </span>;
-            })}
-            </p>
-            <p>{_("Added")}: {modifiedFiles.added.reduce((combined, item) => { combined += item.path + " "; return combined }, "")}</p>
-            <p>{_("Removed")}: {modifiedFiles.removed.reduce((combined, item) => { combined += item.path + " "; return combined }, "")}</p>
-        </>
+            {modifiedFiles.modified.length > 0 &&
+                accordionItem("def-modified-files", _("Modified Files"), (
+                    <p>
+                        {modifiedFiles.modified.map(item => {
+                            if (!item.file_diff) {
+                                return item.path + " ";
+                            }
+                            return <span key={item.path}><a onClick={() => Dialogs.show(<DiffDialog file={item.path} diff={item.file_diff} />)}>{item.path}</a> </span>;
+                        })}
+                    </p>
+                ))}
+            {modifiedFiles.added.length > 0 &&
+                accordionItem("def-added-files", _("Added Files"), (
+                    <p>
+                        {modifiedFiles.added.reduce((combined, item) => { combined += item.path + " "; return combined }, "")}
+                    </p>
+                ))}
+            {modifiedFiles.removed.length > 0 &&
+                accordionItem("def-removed-files", _("Removed Files"), (
+                    <p>
+                        {modifiedFiles.removed.reduce((combined, item) => { combined += item.path + " "; return combined }, "")}
+                    </p>
+                ))}
+        </Accordion>
     );
 };
 
 const SnapshotDiffPage = ({ snapshot1, snapshot2, snapshots }: { snapshot1: number, snapshot2: number, snapshots: Snapshot[] }) => {
     const Dialogs = useDialogs();
-
-    console.log(cockpit.location);
 
     return (
         <>
