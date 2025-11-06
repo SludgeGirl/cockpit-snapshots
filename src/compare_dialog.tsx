4d86f2004cf70c942546fd53362a9934305637f4
Alice Brooks <alice.brooks@suse.com>
2025-11-06 11:20:20 +0000

    Remove SimpleSelect implementation in favour of cockpits and tidy up layout

--- src/compare_dialog.tsx
+++ src/compare_dialog.tsx
@@ -1,69 +1,11 @@
 import cockpit from "cockpit";
-import React, { ReactNode, useCallback, useEffect, useState } from "react";
-import { Button, MenuToggle, MenuToggleElement, Modal, ModalBody, ModalHeader, Select, SelectList, SelectOption } from "@patternfly/react-core";
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, Flex, FlexItem, Modal, ModalBody, ModalHeader, Stack, StackItem } from "@patternfly/react-core";
 import { useDialogs } from "dialogs";
 import { Snapshot } from "./types";
+import { SimpleSelect } from "cockpit-components-simple-select";
 
 const _ = cockpit.gettext;
-
-const SimpleSelect = ({ children, defaultValue, key, onselect }: { children: ReactNode, defaultValue?: string | number | readonly string[] | undefined, key?: string, onselect?: (event: React.MouseEvent<Element, MouseEvent> | undefined, value: string | number | undefined) => void }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [selected, setSelected] = useState<string | number | readonly string[]>('Select a value');
-
-    useEffect(() => {
-        if (defaultValue)
-            setSelected(defaultValue);
-    }, [defaultValue]);
-
-    const onToggleClick = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const toggle = (toggleRef: React.Ref<MenuToggleElement>) => (
-        <MenuToggle
-            ref={toggleRef}
-            onClick={onToggleClick}
-            isExpanded={isOpen}
-            style={
-                {
-                    width: '200px'
-                } as React.CSSProperties
-            }
-        >
-            {selected}
-        </MenuToggle>
-    );
-
-    const onSelect = (event: React.MouseEvent<Element, MouseEvent> | undefined, value: string | number | undefined) => {
-        // eslint-disable-next-line no-console
-        if (onselect) {
-            onselect(event, value);
-        }
-
-        setSelected(value as string);
-        setIsOpen(false);
-    };
-
-    console.log("Default Value", defaultValue);
-
-    return (
-        <Select
-            id={key ?? ""}
-            key={key}
-            isOpen={isOpen}
-            selected={selected}
-            onSelect={onSelect}
-            onOpenChange={(isOpen) => setIsOpen(isOpen)}
-            toggle={toggle}
-            defaultValue={defaultValue}
-            shouldFocusToggleOnSelect
-        >
-            <SelectList>
-                {children}
-            </SelectList>
-        </Select>
-    );
-};
 
 export const CompareDialog = ({ snapshots }: { snapshots: Snapshot[] }) => {
     const Dialogs = useDialogs();
@@ -89,25 +31,46 @@ export const CompareDialog = ({ snapshots }: { snapshots: Snapshot[] }) => {
     return (
         <Modal
             variant="medium"
+            position="top"
             onClose={() => Dialogs.close()}
             isOpen
         >
             <ModalHeader title={_("Compare Snapshots")} />
             <ModalBody>
-                <SimpleSelect key="first-snapshot" defaultValue={firstSelect ?? undefined} onselect={(_, value) => setFirstSelect(Number(value))}>
-                    {snapshots.map((snapshot) =>
-                        <SelectOption key={snapshot.number} value={snapshot.number}>{snapshot.number}</SelectOption>)}
-                </SimpleSelect>
-                With
-                <SimpleSelect key="second-snapshot" defaultValue={secondSelect ?? undefined} onselect={(_, value) => setSecondSelect(Number(value))}>
-                    {snapshots.map((snapshot) =>
-                        <SelectOption key={snapshot.number} value={snapshot.number}>{snapshot.number}</SelectOption>)}
-                </SimpleSelect>
-                <Button
-                    isDisabled={firstSelect === null || secondSelect === null}
-                    onClick={compareSnapshots}
-                >{_("Compare Snapshots")}
-                </Button>
+                <Stack hasGutter>
+                    <StackItem>
+                        <Flex>
+                            <FlexItem>
+                                <SimpleSelect
+                                    key="first-snapshot"
+                                    placeholder={_("Please select a value")}
+                                    onSelect={(value) => setFirstSelect(Number(value))}
+                                    options={snapshots.map((snapshot) => ({ value: snapshot.number, content: snapshot.number }))}
+                                    selected={firstSelect ?? undefined}
+                                />
+                            </FlexItem>
+                            <FlexItem>
+                                <p>{_("With")}</p>
+                            </FlexItem>
+                            <FlexItem>
+                                <SimpleSelect
+                                    key="second-snapshot"
+                                    placeholder={_("Please select a value")}
+                                    onSelect={(value) => setSecondSelect(Number(value))}
+                                    options={snapshots.map((snapshot) => ({ value: snapshot.number, content: snapshot.number }))}
+                                    selected={secondSelect ?? undefined}
+                                />
+                            </FlexItem>
+                        </Flex>
+                    </StackItem>
+                    <StackItem>
+                        <Button
+                            isDisabled={firstSelect === null || secondSelect === null}
+                            onClick={compareSnapshots}
+                        >{_("Compare Snapshots")}
+                        </Button>
+                    </StackItem>
+                </Stack>
             </ModalBody>
         </Modal>
     );
